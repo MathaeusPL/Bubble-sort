d75d591b758f179db94db76dfacb599cbf9dfe17
MathaeusPL <38765179+MathaeusPL@users.noreply.github.com>
2018-05-09 19:16:09 +0200

    Add files via upload

--- src/com/mateusz_famulski/Main.java
+++ src/com/mateusz_famulski/Main.java
@@ -1,5 +1,18 @@
 package com.mateusz_famulski;
 
+/**
+ * Program sortuje pseudolosowe liczby.
+ * Zakres liczb podaje użytkowni.
+ * Projekt zawiera się w dwóch klasach Main oraz Logika.
+ * Program testowany na poprawnych danych wejściowych nie ujawnił błędów.Zastosowano obsługę wyjątków w przypadku
+ * wprowadzenia przez użytkownika błędnego rodzaju danych w tym zbyt małego zakresu losowanych liczb oraz zbyt małej ich ilości do sortowania.
+ * Przetestowano program przy błędnych danych - program dobrze obsługuje błędy.
+ * Program pisany w IntelliJ IDEA na Windows 10. Wersja javy "1.8.0_144".
+ *
+ *  @author Mateusz Famulski
+ *  @version 0.1
+ */
+
 public class Main {
 
     public static void main(String[] args) {
